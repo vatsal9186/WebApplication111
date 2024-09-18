79a3358b8a484d5502ddc0b16ef0e58140f0d6d8
karan9186 <parmarkarans7486@gmail.com>
2024-09-18 15:28:33 +0530

    First Commit

--- WebApplication111/WebForm1.aspx
+++ WebApplication111/WebForm1.aspx
@@ -3,12 +3,23 @@
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
      <h2> home page </h2>
-<p> 
+<%--<p> 
     <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
     <asp:RegularExpressionValidator ID="RegularExpressionValidator1" runat="server" ErrorMessage="hello"></asp:RegularExpressionValidator>
-</p>
+</p>--%>
 <p> &nbsp;</p>
 <p> 
     <asp:Button ID="Button1" runat="server" Text="Button" />
+&nbsp;&nbsp;&nbsp;
+    <asp:Button ID="Button2" runat="server" Text="Button" />
+</p>
+     <p> 
+         &nbsp;</p>
+     <p> 
+         &nbsp;</p>
+     <p> 
+         &nbsp;</p>
+     <p> 
+         <asp:Button ID="Button3" runat="server" Text="Button" />
 </p>
 </asp:Content>
--- WebApplication111/WebForm1.aspx.designer.cs
+++ WebApplication111/WebForm1.aspx.designer.cs
@@ -15,24 +15,6 @@ namespace WebApplication111
     {
 
         /// <summary>
-        /// TextBox1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox TextBox1;
-
-        /// <summary>
-        /// RegularExpressionValidator1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.RegularExpressionValidator RegularExpressionValidator1;
-
-        /// <summary>
         /// Button1 control.
         /// </summary>
         /// <remarks>
@@ -40,5 +22,23 @@ namespace WebApplication111
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Button Button1;
+
+        /// <summary>
+        /// Button2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button2;
+
+        /// <summary>
+        /// Button3 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button3;
     }
 }
